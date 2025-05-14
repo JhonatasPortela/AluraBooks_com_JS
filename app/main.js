96a37ac9463641883efcfad46106f24657130ca2
Jhonatas Portela <jhonatasportela1997@gmail.com>
2025-05-13 21:43:33 -0300

    Aplicando desconto no preço

--- app/main.js
+++ app/main.js
@@ -5,24 +5,6 @@ getBuscarLivrosDaAPI();
 async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointDaAPI);
   livros = await res.json();
-  console.table(livros);
-  exibirLivrosNaTela(livros);
-}
-
-function exibirLivrosNaTela(listaLivros) {
-  const elementoParaInserirLivros = document.getElementById("livros");
-  listaLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `
-    <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>`;
-  });
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirLivrosNaTela(livrosComDesconto);
 }
